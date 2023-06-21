e7e3aecbc43ee365bd0c59db1ebf2091bfff58bd
Adrien Tisseraud <adrien.tisseraud@gmail.com>
2023-06-21 12:57:39 +0200

    Add binding script

---
+++ bindings/remap.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from pymouse import PyMouseEvent
+from pykeyboard import PyKeyboard
+
+k = PyKeyboard()
+class MouseToButton(PyMouseEvent):
+    def click(self, x, y, button, press):
+        if button == 8:
+            if press:    # press
+                k.press_key(k.control_l_key)
+            else:        # release
+                k.release_key(k.control_l_key)
+        elif button == 9:
+            if press:
+                k.press_key(k.shift_l_key)
+            else:
+                k.release_key(k.shift_l_key)
+
+try:
+    C = MouseToButton()
+    C.run()
+except KeyboardInterrupt:
+    print('\nK thx bye!')
+    exit()
